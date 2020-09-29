f72bf8fa28ceb228ce328a5b7f7593cbe211b77f
Gabriel Murakami <gabriel.alves@youse.com.br>
2020-09-29 11:21:00 -0300

    fix correct order to show trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,9 +4,9 @@ class TripsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @trips = Trip.where(user: current_user).select do |trip|
+    @trips = Trip.where(user: current_user).order(finish_time: :desc).select do |trip|
       trip.finish_station.present? && trip.finish_time > Date.current.prev_month
-    end.reverse
+    end
 
     @actual_bill_value = @trips.sum(&:cost)
   end
